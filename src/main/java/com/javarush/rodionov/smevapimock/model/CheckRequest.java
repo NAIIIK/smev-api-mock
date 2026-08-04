0b602c1f0d5b50c141519cbe7a4d6fdb506b43ce
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-04 21:04:51 +0200

    edited Dto entities

--- src/main/java/com/javarush/rodionov/smevapimock/model/CheckRequest.java
+++ src/main/java/com/javarush/rodionov/smevapimock/model/CheckRequest.java
@@ -1,9 +1,18 @@
 package com.javarush.rodionov.smevapimock.model;
 
-public record CheckRequest(
-        String familyName,
-        String firstName,
-        String patronymic,
-        String series,
-        String number
-) {}
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class CheckRequest {
+    private String familyName;
+    private String firstName;
+    private String patronymic;
+    private String series;
+    private String number;
+}
--- src/main/java/com/javarush/rodionov/smevapimock/model/CheckResponse.java
+++ src/main/java/com/javarush/rodionov/smevapimock/model/CheckResponse.java
@@ -1,22 +1,26 @@
 package com.javarush.rodionov.smevapimock.model;
 
-import lombok.Getter;
+import lombok.*;
 
-@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class CheckResponse {
-    private final boolean isValid;
-    private final String status;
-    private final String decodedStatus;
+    public boolean isValid = true;
+    public String status = "300";
+    public String decodeDocStatus = "Паспорт действителен";
+    public String issuerCode = null;
+    public String issueDate = null;
+    public String invalidityReason = null;
+    public String decodeInvalidityReason = null;
+    public String invaliditySince = null;
 
-    public CheckResponse(boolean valid) {
-        this.isValid = valid;
+    public Result result = new Result();
 
-        if (valid) {
-            this.status = "300";
-            this.decodedStatus = "Passport is valid";
-        } else {
-            this.status = "301";
-            this.decodedStatus = "Passport is invalid";
-        }
+    public static class Result {
+        public boolean success = true;
+        public String errorCode = "";
+        public String description = "";
     }
 }
